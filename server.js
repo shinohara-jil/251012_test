87d6289c4d491925b770e559203a68f4c1a27ca9
takahiro <shinohara@jilinc.co.jp>
2025-10-16 19:37:00 +0900

    server更新

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
 dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT || 3000; // ←重要！
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// index.htmlを表示
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.sendFile(join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
